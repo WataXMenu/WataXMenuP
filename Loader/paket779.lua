44d629a5fec6f19077a7ac94a01f58032a034d8d
WataXMenu <galovking9910@gmail.com>
2025-10-06 23:20:13 +0800

    Update paket779.lua

--- Loader/paket779.lua
+++ Loader/paket779.lua
@@ -108,7 +108,7 @@ local pages = {
     ["Fitur Lain"] = {
         {text="Rollback", link="https://raw.githubusercontent.com/WataXScript/WataXCheat/main/Loader/RB.lua"},
         {text="Auto Respawn", link="https://raw.githubusercontent.com/WataXScript/WataXCheat/main/Loader/afk.lua"},
-        {text="Private Server", link""},
+        {text="Private Server", link="https://pastefy.app/4H3HFiK5/raw"},
     },
     ["Fix Lag"] = {
         {text="Potato Mode",  link="https://raw.githubusercontent.com/WataXScript/WataXCheat/main/Loader/Potato.lua"},
